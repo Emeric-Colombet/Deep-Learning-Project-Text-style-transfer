defb96cf3a136bae5e533fdc0f6e10154e9bd3a0
“Emeric <emeric.colombet@2020.icam.fr>
2022-11-16 13:50:15 +0100

    Useless change

--- style_transfer/application/main_train.py
+++ style_transfer/application/main_train.py
@@ -3,11 +3,11 @@ from style_transfer.domain.style_transfer_model import BaseStyleTransferModel
 from style_transfer.domain.prepocess_data import PreprocessData
 std = StyleTransferData('foo.csv','foo2.csv')
 bstm = BaseStyleTransferModel()
-pd = PreprocessData()
+ppd = PreprocessData()
 print(f"Is the loader working? {std.load_clean_data()=='Dataset'}")
 print(f"Is fitting method implemented? {bstm.fit(X=1,y=1)=='fit'}")
 try :
-    pd.transform('df')
+    ppd.transform('df')
 except NotImplementedError:
     print("All right, the transform method is not implemented")
 
