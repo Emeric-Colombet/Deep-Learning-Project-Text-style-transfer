e321489d4ff46bda8d6a83ebb9192430ac43d435
“Emeric <emeric.colombet@2020.icam.fr>
2022-11-16 20:00:55 +0100

    Keep one line between methods

--- style_transfer/interface/app_transfer_style.py
+++ style_transfer/interface/app_transfer_style.py
@@ -7,27 +7,29 @@ from style_transfer.domain.style_transfer_model import BaseStyleTransferModel
 @dataclass
 class TransferStyleApp:
     model : BaseStyleTransferModel
+
     def RUN(self):
         self._head()
         submit, text_submission = self._text_placeholder()
-        self._display_prediction()
         if submit : 
             prediction = self.model.predict(text_submission)
             self._display_prediction(prediction)
 
-
     def transform_style(self,sentence):
         prediction = self.model.predict(sentence)
         return prediction
+
     @staticmethod
     def _configure_page():
         st.set_page_config(
             page_title='Spanish text style transfer',
             page_icon='assets/bandera.jpg')
+    
     @staticmethod
     def _head():
         st.title('Spanish text style transfer')
         st.caption('From Latino to European style')
+    
     @staticmethod
     def _text_placeholder():
         LAM = '''    Dios mío. ¿se quedó a dormir?
@@ -41,6 +43,7 @@ class TransferStyleApp:
         text_submission = st.text_area('Text on which we transfer style', placeholder = LAM,height=150,help="Write here your Latino Spanish text, and we will transform it into European Spanish style! ¡Venga!")
         submit = st.button('Submit')
         return submit,text_submission
+    
     @staticmethod
     def _display_prediction(prediction="Output of our model"):
         st.subheader('Prediction :')
