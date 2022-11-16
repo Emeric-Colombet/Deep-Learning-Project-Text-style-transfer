54a8984d94f4006f00f75bbb6a924645b9811742
“Emeric <emeric.colombet@2020.icam.fr>
2022-11-16 18:53:21 +0100

    Rewrite the main_inference

--- style_transfer/application/main_inference.py
+++ style_transfer/application/main_inference.py
@@ -1,5 +1,5 @@
 from style_transfer.domain.style_transfer_model import BaseStyleTransferModel
-from style_transfer.interface.http_router import HTTPRouter
-model = BaseStyleTransferModel().load()
-http_router = HTTPRouter(model)
-http_router.RUN()
+from style_transfer.interface.app_transfer_style import TransferStyleApp
+model = BaseStyleTransferModel()
+app = TransferStyleApp(model)
+app.RUN()
