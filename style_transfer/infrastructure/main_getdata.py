dcca0845f608c36ef930791b21f71e2352c3b796
Stephanie <stephaniedorta@Stephanies-MacBook-Air.local>
2022-11-17 11:30:56 +0100

    Add dataload main, modify gitignore, update requirements

---
+++ style_transfer/infrastructure/main_getdata.py
@@ -0,0 +1,8 @@
+from style_transfer.infrastructure.style_transfer_data import StyleTransferData
+import os
+
+DATA_PATH_RELATIVE = '../../data'
+data_path = os.path.abspath(DATA_PATH_RELATIVE)
+
+df_all = StyleTransferData().load_clean_data()
+StyleTransferData.print_to_csv(df_all, data_path, 'all_subtitles.csv')
