4db8c3d0c6f092985b22cfaa2a21fd5d90b2fa9c
Stephanie <stephaniedorta@Stephanies-MacBook-Air.local>
2022-11-20 02:59:07 +0000

    add domain: main_preprocess

---
+++ style_transfer/domain/main_preprocess.py
@@ -0,0 +1,6 @@
+from style_transfer.infrastructure.style_transfer_data import StyleTransferData, Subtitles
+from style_transfer.domain.preprocess_data import EuropeanSpanishTerms, BaseData
+
+df = StyleTransferData.data
+df = EuropeanSpanishTerms(df).count_regional_terms()
+df_train, df_validation, df_test = BaseData(df).split_train_test()
