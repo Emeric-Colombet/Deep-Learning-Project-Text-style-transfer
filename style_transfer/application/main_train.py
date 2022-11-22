4dad81477e23b9d0af3488ed9eed938d4ecba2f4
“Emeric <emeric.colombet@2020.icam.fr>
2022-11-22 18:49:21 +0100

    Modified main train

--- style_transfer/application/main_train.py
+++ style_transfer/application/main_train.py
@@ -2,21 +2,9 @@ import logging
 from style_transfer.infrastructure.style_transfer_data import StyleTransferData
 from style_transfer.domain.style_transfer_model import BaseStyleTransferModel, TransformerStyleTransferModel
 from style_transfer.domain.preprocess_data import PreprocessData
-
 from style_transfer.infrastructure.style_transfer_data import StyleTransferData
 from style_transfer.domain.preprocess_data import EuropeanSpanishTerms, BaseData
-
-
-bstm = BaseStyleTransferModel()
-ppd = PreprocessData()
-
-print(f"Is fitting method implemented? {bstm.fit(X=1,y=1)=='fit'}")
-try :
-    ppd.transform('df')
-except NotImplementedError:
-    print("All right, the transform method is not implemented")
-logging.getLogger().setLevel(logging.INFO)
-logging.info("Loading data")
+from evaluate.evaluator import Text2TextGenerationEvaluator
 df = StyleTransferData.data
 df = EuropeanSpanishTerms(df).count_regional_terms()
 df_train, df_validation, df_test = BaseData(df).split_train_test(test_size=0.15, validation_size=0.2, random_state=42)
@@ -24,5 +12,7 @@ df_train, df_validation, df_test = BaseData(df).split_train_test(test_size=0.15,
 df_train["combined"] = '<s>' + df_train["text_latinamerica"] + '</s>' + '>>>>' + '<p>' + df_train["text_spain"] + '</p>'
 df_validation["combined"] = '<s>' + df_validation["text_latinamerica"] + '</s>' + '>>>>' + '<p>' + df_validation["text_spain"] + '</p>'
 logging.info("Data loaded")
-gpt = TransformerStyleTransferModel('style_transfer/application/combine.txt',1,model_name='flax-community/gpt-2-spanish',batch_size=8,cache_dir='cache',output_dir='Latino_to_European')
+gpt = TransformerStyleTransferModel(1,model_name="Latino_to_European",tokenizer_name='flax-community/gpt-2-spanish',batch_size=8,cache_dir='cache',output_dir='Latino_to_European')
 gpt.fit(df_train,df_validation)
+logging.info("Model fitted")
+Text2TextGenerationEvaluator.compute()
